2700e7d95a47e35166052d748df54c1884f691d2
shisckatchegg <alvaro_martinez_93@hotmail.com>
2014-12-06 17:48:43 +0100

    Buttonscript y TrampaTecho incluidos Placeholder para el boton puesto en pantalla

---
+++ project/Assets/Scripts/Buttonscript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttonscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { }
+
+
+	// Update is called once per frame
+    void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Pressed left click, casting ray.");
+			CastRay();
+		}
+	}
+
+
+
+	bool CastRay() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, Mathf.Infinity);
+		if (hit){
+			Debug.Log(hit.collider.gameObject.name);
+			return true;
+        }
+    }    
+}
