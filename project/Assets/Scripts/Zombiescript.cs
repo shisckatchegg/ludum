35c57f4eb93674bf3890f40397a8c02f4888cb5a
Tapisama <fratapmer@gmail.com>
2014-12-06 20:45:36 +0100

    Arreglado Zombiescript

--- project/Assets/Scripts/Zombiescript.cs
+++ project/Assets/Scripts/Zombiescript.cs
@@ -8,7 +8,7 @@ public class Zombiescript : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D objeto) {
-
+		trampa=objeto.gameObject.GetComponent("Trampscript") as Trampscript;
 		if (objeto.gameObject.tag=="Trampa" && trampa.activate){
 			//añadir espera para animación mas adelante
 			Destroy(this.gameObject);
