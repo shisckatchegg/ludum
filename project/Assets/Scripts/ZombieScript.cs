2143edd3c13fa3905d31adae5d021ff944371be0
shisckatchegg <alvaro_martinez_93@hotmail.com>
2014-12-06 20:22:54 +0100

    Scene arreglado Nombre de script arreglado (Se supone)

--- project/Assets/Scripts/ZombieScript.cs
+++ project/Assets/Scripts/ZombieScript.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class Zombiescript : MonoBehaviour {
-
+	private Trampscript trampa;
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
-	void OnCollisionEnter2D(Collision2D objeto){
-		if (objeto.gameObject.tag=="Trampa" && objeto.gameObject.GetComponent("TrampScript") as TrampScript.activate){
+
+	void OnCollisionEnter2D(Collision2D objeto) {
+
+		if (objeto.gameObject.tag=="Trampa" && trampa.activate){
 			//añadir espera para animación mas adelante
 			Destroy(this.gameObject);
 		}
