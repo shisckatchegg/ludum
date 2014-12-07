9003c8f334d310ad7b7b9278717b8d6474d4d01e
Tapisama <fratapmer@gmail.com>
2014-12-07 21:15:28 +0100

    COntador de tiempo

---
+++ project/Assets/Scripts/Countdownscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdownscript : MonoBehaviour {
+	private float timer=300;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer=timer-Time.deltaTime;
+		guiText.text = timer.ToString("F0");
+	}
+	
+	
+}
