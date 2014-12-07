60cd0e83d1c6be39b890a1b9fa1a5d93540d488b
shisckatchegg <alvaro_martinez_93@hotmail.com>
2014-12-08 00:30:35 +0100

    Menu ingame 2

--- project/Assets/Scripts/Scenecontrol.cs
+++ project/Assets/Scripts/Scenecontrol.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class Scenecontrol : MonoBehaviour {
 	GameObject timer;
+	private bool added = false;
+
 	// Use this for initialization
 	void Start () {
-		timer=GameObject.Find("Timer");
+		timer = GameObject.Find("Timer");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if(timer.GetComponent<Countdownscript>().timer<=0){
-			Application.LoadLevel("Menu");
+
+		if(timer.GetComponent<Countdownscript>().timer <= 0 && !added) {
+			//Application.LoadLevel("Menu");
+			//if(transform.parent.gameObject.GameOverscript){
+			gameObject.AddComponent<GameOverscript>();
+			added = true;			//}	
+
 		}
 	}
 }
