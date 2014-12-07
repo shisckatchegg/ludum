7692da82ac62053f07fb1fc0e09cedc7cf0de269
shisckatchegg <alvaro_martinez_93@hotmail.com>
2014-12-07 12:52:43 +0100

    Menu en desarrollo

---
+++ project/Assets/Scripts/Menuscript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menuscript : MonoBehaviour{
+	
+	void OnGUI () {
+		const int buttonWidth = 84;
+		const int buttonHeight = 60;
+
+		Rect buttonRect = new Rect (Screen.width / 2 - (buttonWidth/2), (2*Screen.height/3) - (buttonHeight/2), buttonWidth, buttonHeight);
+
+		if (GUI.Button (buttonRect, "Start!")) {
+			Application.LoadLevel("main");	
+		}
+	}
+}
