6542377ca4fc27064316e9086e7d751f3fb0af01
Tapisama <fratapmer@gmail.com>
2014-12-08 00:45:34 +0100

    ZOmbiespawn añadido

---
+++ project/Assets/Scripts/Zombiespawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombiespawn : MonoBehaviour {
+	public int numZombies=30;
+	public GameObject spawn;
+	// Use this for initialization
+	void Start () {
+		for(int i=0;i<numZombies;i++){
+			spawnZombie();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void spawnZombie(){
+		Vector3 position = new Vector3(Random.Range(-10.0F, -5.0F), 0, 0);
+		Instantiate(spawn,position,Quaternion.identity);
+	}
+}
