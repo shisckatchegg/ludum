d094b2f157d562e65c6877f28de19a992e4273b9
shisckatchegg <alvaro_martinez_93@hotmail.com>
2014-12-08 01:01:04 +0100

    Menu in-game 2

--- project/Assets/Scripts/GameOverscript.cs
+++ project/Assets/Scripts/GameOverscript.cs
@@ -7,42 +7,7 @@ public class GameOverscript : MonoBehaviour {
 	const int buttonHeight = 60;
 
 	void Update (){
-					Debug.Log("ENTRA?");
-		if (Input.GetKeyDown ("escape")) {
-			Debug.Log("SALE?");
-			if (
-			GUI.Button(
-			// Center in X, 1/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(1 * Screen.height / 3) - (buttonHeight / 2),
-			buttonWidth,
-			buttonHeight
-			),
-			"Retry!"
-			)
-			) {
-				// Reload the level
-				Application.LoadLevel("main");
-			}
-
-			if (
-			GUI.Button(
-			// Center in X, 2/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 3) - (buttonHeight / 2),
-			buttonWidth,
-			buttonHeight
-			),
-			"Back to menu"
-			)
-			){
-				// Reload the level
-				Application.LoadLevel("Menu");
-			}
-				
-		}
+	
 	}
 	
 	
--- project/Assets/Scripts/Scenecontrol.cs
+++ project/Assets/Scripts/Scenecontrol.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class Scenecontrol : MonoBehaviour {
 	GameObject timer;
+	GameObject killCount;
 	private bool added = false;
+	public int winCondition = 10;
 
 	// Use this for initialization
 	void Start () {
 		timer = GameObject.Find("Timer");
+		killCount = GameObject.Find("killCount");
+		guiText.enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
 		if(timer.GetComponent<Countdownscript>().timer <= 0 && !added) {
-			//Application.LoadLevel("Menu");
-			//if(transform.parent.gameObject.GameOverscript){
+			guiText.enabled = true;
+			guiText.text = "You failed defending the laboratory!";
 			gameObject.AddComponent<GameOverscript>();
-			added = true;			//}	
+			added = true;
+		}
+
+		if(killCount.GetComponent<Killedcounterscript>().killed >= winCondition && !added) {
+			guiText.enabled = true;
+			guiText.text = "Laboratory defended succesfully!";
+			gameObject.AddComponent<GameOverscript>();
+			added = true;
 
 		}
 	}
